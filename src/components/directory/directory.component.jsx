6e270b52cac7d0ee39742ba62e1714b4e6abd72b
ppuuffyy <ppuuffyy@gmail.com>
2020-04-06 18:32:31 +0300

    added React Router DOM

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -51,10 +51,15 @@ class  Directory extends React.Component {
         return (
         <div className='directory-menu'>
             {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size} />
-                )
-                )
+                // this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => (
+                //     <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
+                // )
+                // )
+
+              this.state.sections.map(({id, ...otherSectionProps}) => (
+                  <MenuItem key={id} {...otherSectionProps}/>
+              )
+              )
             }
 
 
