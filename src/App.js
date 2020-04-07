4453ac3241c4f86d9a5d4fb80e4ef11435f0dc96
ppuuffyy <ppuuffyy@gmail.com>
2020-04-07 06:17:09 +0300

    added to shop page collection-preview and collection-item components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+
 
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
     <div>
       <Switch>
         <Route exact  path='/'   component={HomePage} />
-        <Route exact path='/hats'  component={HatsPage} />
+        <Route exact path='/shop'  component={ShopPage} />
       </Switch>
 
 
