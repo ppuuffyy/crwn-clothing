88d36f468fec3e9b9093dc92620a5a49fbd8da22
ppuuffyy <ppuuffyy@gmail.com>
2020-04-10 08:36:17 +0300

    added category page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
 import CollectionItem from '../collection-item/collection-item.component';
-import './collection-preview.styles.scss'
+import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({title, routeName, items, history, match}) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
+        <h1 className='title' onClick={() => history.push(`${match.url}/${routeName}`)} >{title.toUpperCase()}</h1>
         <div className='preview'>
             {
                 items.filter((item,idx) => idx<4).map((item) => (
@@ -16,4 +17,4 @@ const CollectionPreview = ({title, items}) => (
 
 )
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import {Switch, Route} from 'react-router-dom';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+const ShopPage = ({match}) => {
 
-    render(){
-        const {collections} = this.state;
-       
+        
         return (
-            <div className='shop-page'> {
-                collections.map( ({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))}
+            <div className='shop-page'> 
+                <Switch>
+                    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+                </Switch>
             </div>
         );
     }
 
-}
 
 export default ShopPage;
